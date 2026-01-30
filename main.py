dcd56ad862c9d71c28add46d37616ba5b1922ee6
KUNAL PRAMANIK <pramanikkunal65@gmail.com>
2026-01-30 15:48:38 +0530

    Kunal & Jinal

--- main.py
+++ main.py
@@ -40,3 +40,45 @@ def get_faculty():
             """)
         )
         return result.mappings().all()
+
+
+
+@app.get(
+    "/faculty-details-by-name",
+    response_model=List[FacultyResponse],
+    summary="Get details by faculty name",
+    description="Enter a faculty name to retrieve their details"
+)
+def get_research_area_by_name(
+    name: str = Query(..., description="Full name of the faculty")
+):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("""
+                SELECT *
+                FROM faculty
+                WHERE name = :name
+            """),
+            {"name": name}
+        )
+        return result.mappings().all()
+
+
+@app.get(
+    "/search-by-research-area",
+    response_model=List[str],
+    summary="Search faculty by research area keyword"
+)
+def search_faculty_by_research_area(
+    keyword: str = Query(..., description="Keyword like Machine Learning")
+):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("""
+                SELECT name
+                FROM faculty
+                WHERE LOWER(research_area) LIKE LOWER(:keyword)
+            """),
+            {"keyword": f"%{keyword}%"}
+        )
+        return [row["name"] for row in result.mappings()]
